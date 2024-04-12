52ce0211e0facceb52a0f1af2d78692b6391dbeb
Loyoenock <loyoenock@gmail.com>
2024-04-12 12:34:29 +0300

    Component - Hero section added

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -1,9 +1,35 @@
+import qs from "qs";
+import { flattenAttributes } from "@/lib/utils"
+import { HeroSection } from "@/components/ui/custom/HeroSection";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"]          
+        },
+        link: {
+          populate: true
+        }
+      }
+    }
+  }
+});
+
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
   try { 
-    const response = await fetch(baseUrl + path);
+    const response = await fetch(url.href);
+
+    // console.log(url)
     const data = await response.json();
-    return data;
+    const flattenedData = flattenAttributes(data);
+    // console.dir(flattenedData, { depth: null });
+    return flattenedData;
   } catch (error) {
     console.error(error);
   } 
@@ -13,12 +39,11 @@ export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
   console.log(strapiData);
 
-  const { Title, Description } = strapiData.data.attributes;
+  const { title, description, blocks } = strapiData;
 
   return (
-   <main className="container mx-auto py-6">
-    <h1 className="text-4xl font-bold text-center">{Title}</h1>
-    <p className="text-xl mt-4 text-center">{Description}</p>
+   <main>
+    <HeroSection  data={blocks[0]} />
    </main>
   );
 }
