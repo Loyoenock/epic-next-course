0c44c292c9e6cf3bcdd7fb302cd931ecbf26c2d1
Loyoenock <loyoenock@gmail.com>
2024-04-12 08:45:03 +0300

    Backend - fetch data from the strapi

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -1,9 +1,24 @@
-import { Button } from "@/components/ui/button";
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  try { 
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  } 
+}
 
-export default function Home() {
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  console.log(strapiData);
+
+  const { Title, Description } = strapiData.data.attributes;
+
   return (
    <main className="container mx-auto py-6">
-    <Button>Our Cool Buttoon </Button>
+    <h1 className="text-4xl font-bold text-center">{Title}</h1>
+    <p className="text-xl mt-4 text-center">{Description}</p>
    </main>
   );
 }
