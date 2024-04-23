c377fb22ab463fb32b6d2a012ca035ae6d34f8cb
Loyoenock <loyoenock@gmail.com>
2024-04-23 08:15:56 +0300

    Protected route added using middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
+
+export async function middleware(request: NextRequest) {
+  const user = await getUserMeLoader();
+  const currentPath = request.nextUrl.pathname;
+
+  console.log("################## MIDDLEWARE ##############")
+  console.log(user);
+  console.log(currentPath)
+  console.log("################## MIDDLEWARE ##############")
+
+  if (currentPath.startsWith("/dashboard") && user.ok === false) {
+    const signInUrl = new URL("/signin", request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
+}
